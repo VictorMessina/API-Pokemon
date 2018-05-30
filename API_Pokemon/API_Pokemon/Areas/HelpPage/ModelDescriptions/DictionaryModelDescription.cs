b7bab941626c22b7f094ad5eb53c169b1438eb55
Victor Messina <victormessina95@hotmail.com>
2018-05-30 17:14:00 -0300

    add project

---
+++ API_Pokemon/API_Pokemon/Areas/HelpPage/ModelDescriptions/DictionaryModelDescription.cs
@@ -0,0 +1,6 @@
+namespace API_Pokemon.Areas.HelpPage.ModelDescriptions
+{
+    public class DictionaryModelDescription : KeyValuePairModelDescription
+    {
+    }
+}
---
+++ API_Pokemon/API_Pokemon/Areas/HelpPage/ModelDescriptions/SimpleTypeModelDescription.cs
@@ -0,0 +1,6 @@
+namespace API_Pokemon.Areas.HelpPage.ModelDescriptions
+{
+    public class SimpleTypeModelDescription : ModelDescription
+    {
+    }
+}
