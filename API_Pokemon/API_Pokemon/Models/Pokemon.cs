be32bed9d764f7179f2cf39531636a3d205f4a77
Victor Messina <victormessina95@hotmail.com>
2018-05-30 17:47:52 -0300

    added new class Pokemon

---
+++ API_Pokemon/API_Pokemon/Models/Pokemon.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+using Newtonsoft.Json;
+
+namespace API_Pokemon.Models
+{
+    [Serializable]
+    public class Pokemon
+    {
+        [DatabaseGenerated(DatabaseGeneratedOption.Identity)]
+        public int PokemonID { get; set; }
+
+        [Required]
+        public int NationalPokedexNumber { get; set; }
+
+        [Required]
+        public string Name { get; set; }
+
+        [Required]
+        public string Generation { get; set; }
+
+        [Required]
+        public bool Evolve { get; set; }
+
+        [Required]
+        public bool Rare { get; set; }
+
+        [Required]
+        public string Image { get; set; }
+
+        [Required]
+        public int RegionID { get; set; }
+
+        [JsonIgnore]
+        public virtual Region Region { get; set; }
+
+        [Required]
+        public int PokemonTypeID { get; set; }
+
+        [JsonIgnore]
+        public virtual PokemonType PokemonType { get; set; }
+    }
+}
